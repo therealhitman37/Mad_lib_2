c7005377748d606c672348da8e53a4271cfdbe1c
therealhitman37 <102584348+therealhitman37@users.noreply.github.com>
2022-04-12 00:56:02 +0200

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,8 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ") 
+verb2 = input("Verb: ")
+famous_person = input("famous person")
+
+madlib = f"Computer progamming is so {adj}! It makes me so ecited all the time because \n i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
